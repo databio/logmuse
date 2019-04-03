0f2f8a310bbb6169f3d5cfb5c871ec516b9be7b6
Vince Reuter <vince.reuter@gmail.com>
2019-04-03 12:02:23 -0400

    renaming

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ import sys
 from setuptools import setup
 
 
-PKG = "infomuse:
+PKG = "logmuse"
 REQDIR = "requirements"
 
 
 def read_reqs(reqs_name):
     deps = []
-    with open(os.path.join(REQDIR, "requirements-{}.txt".format(reqs_name)), 'r') as f:
+    fp = os.path.join(REQDIR, "requirements-{}.txt".format(reqs_name))
+    with open(fp, 'r') as f:
         for l in f:
             if not l.strip():
                 continue
-            #deps.append(l.split("=")[0].rstrip("<>"))
             deps.append(l)
     return deps
 
@@ -24,35 +24,11 @@ extra = {}
 # Ordinary dependencies
 DEPENDENCIES = read_reqs("all")
 
-# numexpr for pandas
-try:
-    import numexpr
-except ImportError:
-    # No numexpr is OK for pandas.
-    pass
-else:
-    # pandas 0.20.2 needs updated numexpr; the claim is 2.4.6, but that failed.
-    DEPENDENCIES.append("numexpr>=2.6.2")
-
 # 2to3
 if sys.version_info >= (3, ):
     extra["use_2to3"] = True
 extra["install_requires"] = DEPENDENCIES
 
-
-# Additional files to include with package
-def get_static(name, condition=None):
-    static = [os.path.join(name, f) for f in os.listdir(
-            os.path.join(os.path.dirname(os.path.realpath(__file__)), name))]
-    if condition is None:
-        return static
-    else:
-        return [i for i in filter(lambda x: eval(condition), static)]
-
-
-# scripts to be added to the $PATH
-# scripts = get_static("scripts", condition="'.' in x")
-scripts = None
 
 with open("{}/_version.py".format(PKG), 'r') as versionfile:
     version = versionfile.readline().split()[-1].strip("\"'\n")
@@ -84,7 +60,7 @@ setup(
     url="https://github.com/vreuter/{}/".format(PKG),
     author=u"Vince Reuter",
     license="BSD2",
-    scripts=scripts,
+    scripts=None,
     include_package_data=True,
     test_suite="tests",
     tests_require=read_reqs("dev"),
